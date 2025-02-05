86c345d31db7186f4f3021dd517c4a3a9ce5bcab
Sunjoo Park <all4dich@gmail.com>
2025-02-05 17:23:06 +0900

    backup

--- lcd/display-character-2.py
+++ lcd/display-character-2.py
@@ -1,21 +1,22 @@
 from RPLCD.gpio import CharLCD
 from RPi import GPIO
 from datetime import datetime
+import time
 GPIO.setmode(GPIO.BCM)
 # Initialize the LCD using lgpio
 lcd = CharLCD(numbering_mode=GPIO.BCM, cols=16, rows=2, pin_rs=27, pin_e=17, pins_data=[25,24,23,22])
 # Write a message
-lcd.clear()
-lcd.write_string("Hello, World!")
-lcd.clear()
 #lcd.write_string("welcome, sunjoo")
-now = datetime.now()
-date_str = str(now.strftime("%D"))
-time_str = str(now.strftime("%T"))
-lcd.cursor_pos = (0,0)
-lcd.write_string("Date: " + date_str)
-lcd.cursor_pos = (1,0)
-lcd.write_string("Time: " + time_str)
+while True:
+    lcd.clear()
+    now = datetime.now()
+    date_str = str(now.strftime("%D"))
+    time_str = str(now.strftime("%T"))
+    lcd.cursor_pos = (0,0)
+    lcd.write_string("Date: " + date_str)
+    lcd.cursor_pos = (1,0)
+    lcd.write_string("Time: " + time_str)
+    time.sleep(2)
 lcd.close()
-GPIO.cleanup()
+#GPIO.cleanup()
 # Clean up after use
