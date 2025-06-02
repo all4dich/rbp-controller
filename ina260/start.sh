83cbb137f342d1c528d95405d96c5751522118b0
Sunjoo Park <all4dich@gmail.com>
2025-06-02 10:19:54 +0900

    feat: Use argument parser for command line arguments for a script

--- ina260/start.sh
+++ ina260/start.sh
@@ -5,4 +5,4 @@ cd "$(dirname "$0")"
 
 # Run the Python script
 . /home/sunjoo/work/rbp-controller/venv/bin/activate
-python3 ina260-data-exporter.py "$@"
+python3 ina260-data-exporter.py --device nvidia-agx-orig "$@"
